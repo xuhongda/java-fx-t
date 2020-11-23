0257834b3d37772e59b94c5dd0345c0759a3fd57
xuhongda <xuhongda7@outlook.com>
2020-11-23 16:38:55 +0800

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        // 实例化按钮
+        Button button = new Button("this LyApp");
+        // 创建布局控件
+        StackPane stackPane = new StackPane();
+
+        // 将button添加到布局
+        stackPane.getChildren().add(button);
+
+        // 创建场景 宽=400 高=400
+        Scene scene = new Scene(stackPane, 400, 400);
+
+        // 将场景添加到窗口
+        primaryStage.setScene(scene);
+
+        // 显示窗口
+        primaryStage.show();
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+}
