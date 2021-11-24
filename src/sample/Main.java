e28391b2ba4fe216338095064d76bee2c092dd3d
xuhongda <xuhongda7@outlook.com>
2021-11-24 21:57:06 +0800

    rename

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Ly");
+        primaryStage.setTitle("java soft");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
         // 实例化按钮
